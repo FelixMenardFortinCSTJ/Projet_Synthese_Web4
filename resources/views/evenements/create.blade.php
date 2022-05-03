89277597cd819d266099476bbd0fbaa08cee585c
FélixMénardFortin <1955254@cstj.qc.ca>
2022-05-03 14:44:54 -0400

    Ajout du code dans le fichier form ainsi que le fichier create des cinq pages d'evenements.

--- resources/views/evenements/create.blade.php
+++ resources/views/evenements/create.blade.php
@@ -1,3 +1,6 @@
 <form action="{{route('evenements.store')}}" method="post">
-    
+    @include('evenements.form')
+        <div>
+            <button type="submit">Ajouter</button>
+        </div>    
 </form>
