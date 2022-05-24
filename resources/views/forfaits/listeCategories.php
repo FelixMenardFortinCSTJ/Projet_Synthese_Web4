1018bf1a750e402b4bd491225a591ad1dcaa6ad1
FélixMénardFortin <1955254@cstj.qc.ca>
2022-05-24 14:52:05 -0400

    modification de listeCategories dans folder forfaits.

--- resources/views/forfaits/listeCategories.php
+++ resources/views/forfaits/listeCategories.php
@@ -6,7 +6,7 @@ Forfait
 
 @section('contenu')
 <h1>Forfaits</h1>
-<a href="{{route('forfaits.categorie.show', 'Aventure')}}">Aventure</a>
+<a href="{{route('forfaits.categorie.show', 'Escapade')}}">Escapade</a>
 <a href="{{route('forfaits.categorie.show', 'Famille')}}">Famille</a>
 <a href="{{route('forfaits.categorie.show', 'Découverte')}}">Découverte</a>
 <a href="{{route('forfaits.categorie.show', 'Détente')}}">Détente</a>
