2d96d27184edd5b4781aea4d4ad39ab3eec8b4bf
GuillaumeLauzon <2050798@cstj.qc.ca>
2022-05-31 08:28:24 -0400

    javascript du panier

--- public/js/Panier.js
+++ public/js/Panier.js
@@ -8,19 +8,63 @@
    	 * Vérification du panneau
      */
 
-        static Panier(){
+    static togglePanneau(panneau, etat){
+        panneau.classList.toggle('panneau-close', !etat);
+        panneau.classList.toggle('panneau-open', etat);
+    }
 
-            var Fermer = document.getElementById("Fermer");
+    static Panier(){
 
-            Fermer.addEventListener("click", e=>{
-                console.log("ferme le panier");
-            });
-        }
+        var Fermer = document.getElementById("Fermer");
+
+        Fermer.addEventListener("click", e=>{
+            console.log("ferme le panier");
+        });
+
+/////////////////////////////////////////////////////////////////////////////
+
+        if (panneau.classList.contains("isPanneau")) {
+			panneau.addEventListener("focus", (e) => {
+				this.togglePanneau(panneau, true);
+				
+				panneau.addEventListener('transitionend', e=>{
+					console.log(e);
+				}, {once:true});
+
+				bouton.style.opacity = '0.2';
+				bouton.style.cursor = 'default';
+			});
+
+			panneau.addEventListener("blur", (e) => {
+				if(e.relatedTarget && e.relatedTarget.tagName === 'A') return;
+				this.togglePanneau(panneau, false);
+				bouton.style.removeProperty('opacity');
+				bouton.style.removeProperty('cursor');
+			});
+
+			panneau.querySelector('.Panier-Open').addEventListener('click', e =>{
+				panneau.blur();
+			});
+
+			panneau.querySelector('.Panier-Close').addEventListener('mousedown', e =>{
+				if(panneau.classList.contains('.Panier-Open')){
+					e.preventDefault(); 
+					e.stopPropagation();
+					panneau.blur();
+				}
+			});
+		}
+    }
 
     /** * * * * * * * * * * * * * * * * * * * * * * * * * * *
    	 * Initialisation du code au chargement de la page
      */
 	static init() {
-			this.Panier();
+        window.addEventListener("load", () => {
+            this.btn_toggle = document.querySelector(".Panier-Open");
+            this.navPan = document.querySelector('.Panier');
+            
+            this.Panier(this.navPan, this.btn_toggle);
+        });
 	}
 }
