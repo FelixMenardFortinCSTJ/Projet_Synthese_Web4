a39c19d0a41bc1cc6a615c1a35694a07bf8bbca7
OlivierBerardino <1685394@cstj.qc.ca>
2022-06-02 12:39:51 -0400

    ajout changement alex

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard.dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
