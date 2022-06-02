610f4dae6e4098b94c9d0b4c59762303cd7eee99
AlexDupere <1950715@cstj.qc.ca>
2022-06-02 12:52:59 -0400

    Création de la branche admin et profil

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,3 +1,11 @@
+@extends('meslayouts.base')
+
+@section('titre')
+Tableau de bord admin
+@endsection
+
+@section('contenu')
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -9,12 +17,35 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <!-- ///////////PROFIL///////////// -->
+                    <h1>Profil</h1>
+                    <div class="Infos">
+                        <div class="nom">
+                            <h3>Nom : </h3>
+                            <p>Nom</p>
+                        </div>
+                        <div class="Courriel">
+                            <h3>Adresse courriel : </h3>
+                            <p>Adresse courriel</p>
+                        </div>
+                        <div class="Adresse">
+                            <h3>Adresse complète : </h3>
+                            <p>Adresse complète</p>
+                        </div>
+                        <div class="Telephone">
+                            <h3>Numéro de téléphone : </h3>
+                            <p>Numéro de téléphone</p>
+                        </div>
+                    </div>
+                    <!-- ////////ADMIN//////// -->
                     @if(Auth::user()->role >5)
-                    <a href="{{route('admin.evenements.index')}}">allo</a>
+                    <a href="{{route('admin.evenements.index')}}">Les évènements</a>
+                    <a href="{{route('admin.evenements.create')}}">Modifier évènements</a>
                     @endif
+                    <!-- //////////////////////// -->
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+@endsection
