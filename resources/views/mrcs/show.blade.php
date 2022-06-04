5d675ed9c4d450241e1b1ade7124cfaad2984ce9
SabrinaCanacMarquis <2039264@cstj.qc.ca>
2022-06-03 20:53:06 -0400

    ajustement de catergorie

--- resources/views/mrcs/show.blade.php
+++ resources/views/mrcs/show.blade.php
@@ -7,16 +7,6 @@ mrc
 @section('contenu')
 <div class="nom">{{ $mrc->nom }}</div>
 <div class="desccription DescriptionMRC">{{ $mrc->description }}</div>
-<div class="image"><img src="{{$mrc->svg}}" alt=""></div>
-<!-- <div class="MapRegion">
-            <img class="mapMRC" src="{{asset('_assets/svg/placeholder.svg')}}" alt="MapMRC">
-        <div class="Reconnue">
-            <h2>Notre région est reconnue pour</h2>
-            <p>Insérer Cahier Contenu p.10-18</p>
-            <p>Insérer Cahier Contenu p.10-18</p>
-            <p>Insérer Cahier Contenu p.10-18</p>
-            <p>Insérer Cahier Contenu p.10-18</p>
-            <p>Insérer Cahier Contenu p.10-18</p>
-        </div> -->
+<div class="image"><img src="{{asset($mrc->svg)}}" alt=""></div>
 
 @endsection
